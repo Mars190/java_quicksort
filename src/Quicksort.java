0db2c3c71905bd4fc8e363bdefad8ef329863845
Mars190 <105299503+Mars190@users.noreply.github.com>
2023-08-01 19:30:32 +0200

    Cleanup

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -4,15 +4,11 @@ public class Quicksort {
     public static void main(String[] args) {
         int[] array = {2, 9, 3, 2, 0, 5, 2, 0, 8, 1};
         System.out.printf("Array before quicksort: %s\n", Arrays.toString(array));
-        quicksort(array);
+        quicksort(array, 0, array.length - 1);
         System.out.printf("Array after quicksort: %s", Arrays.toString(array));
     }
 
-    private static void quicksort(int[] array) {
-        quicksortRecursive(array, 0, array.length - 1);
-    }
-
-    private static void quicksortRecursive(int[] array, int start, int end) {
+    private static void quicksort(int[] array, int start, int end) {
         if (start >= end) {
             return;
         }
@@ -37,8 +33,8 @@ public class Quicksort {
 
         swap(i, end, array); // Pivot an seine endgültige Position bringen
 
-        quicksortRecursive(array, start, i - 1);
-        quicksortRecursive(array, i + 1, end);
+        quicksort(array, start, i - 1);
+        quicksort(array, i + 1, end);
     }
 
     private static void swap(int x, int y, int[] array) {
@@ -47,17 +43,3 @@ public class Quicksort {
         array[y] = temp;
     }
 }
-// ### Start ### //
-// i = 1. Iterator; j = 2. Iterator; p = Pivot;
-// p startet ganz hinten (länge - 1); i startet index 0; j startet index p - 1
-
-// ### Vorgehen ### //
-// i läuft nach rechts, bis zahl > p
-// j läuft nach links, bis zahl < p
-// Wenn i, j jeweils Zahl gefunden tauschen die Zahlen ihren Platz
-// Weiter bis Überkreuzung, iterierung zuende
-
-// ### Besonderes ### //
-// wenn i & j sich gekreuzt haben = iterierung ende -> check ob [i] > [p], dann plätze tauschen
-// pivot nach iterierung = sortiert
-// einzelnes element in der teilliste = sortiert
